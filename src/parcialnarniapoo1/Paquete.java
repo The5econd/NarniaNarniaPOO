298a08e60d60cffb97450771869e3bfcbac482df
NeoCat98 <00076017@uca.edu.sv>
2018-05-02 23:43:31 -0600

    Se creo la clase paquete con sus respectivos atributos,setter,getters y constructores

---
+++ src/parcialnarniapoo1/Paquete.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parcialnarniapoo1;
+
+/**
+ *
+ * @author javie
+ */
+public class Paquete {
+    String nombrePaquete;
+    String contenidoPaquete;
+    double precioPaquete;
+
+    public Paquete(String nombrePaquete, String contenidoPaquete, double precioPaquete) {
+        this.nombrePaquete = nombrePaquete;
+        this.contenidoPaquete = contenidoPaquete;
+        this.precioPaquete = precioPaquete;
+    }
+
+    public Paquete() {
+    }
+    
+    public String getNombrePaquete() {
+        return nombrePaquete;
+    }
+
+    public void setNombrePaquete(String nombrePaquete) {
+        this.nombrePaquete = nombrePaquete;
+    }
+
+    public String getContenidoPaquete() {
+        return contenidoPaquete;
+    }
+
+    public void setContenidoPaquete(String contenidoPaquete) {
+        this.contenidoPaquete = contenidoPaquete;
+    }
+
+    public double getPrecioPaquete() {
+        return precioPaquete;
+    }
+
+    public void setPrecioPaquete(double precioPaquete) {
+        this.precioPaquete = precioPaquete;
+    }
+    
+}
