5f6ae70a286462e2f050e33cd235b2aa90bac491
Fer Salazar <00017917@uca.edu.sv>
2018-05-02 21:09:49 -0600

    SE agrego la clase main

---
+++ src/parcialnarniapoo1/ParcialNarniaPoo1.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parcialnarniapoo1;
+
+/**
+ *
+ * @author thesecond
+ */
+public class ParcialNarniaPoo1 {
+
+    public static void main(String[] args) {
+        AdminHotel Admin = new AdminHotel();
+    }
+    
+}
