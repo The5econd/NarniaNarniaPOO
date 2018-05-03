9e7039c85158cac163454e806229d6248c0f72c7
feranstirman <0024117@uca.edu.sv>
2018-05-02 22:06:36 -0600

    Se agrego la clase AdminHotel con la funcion CrearHabitaciones

---
+++ src/parcialnarniapoo1/AdminHotel.java
@@ -0,0 +1,103 @@
+
+package parcialnarniapoo1;
+import java.util.ArrayList;
+/**
+ *
+ * @author Fer Anstirman
+ */
+public class AdminHotel {
+    
+    private ArrayList<Cliente> clientes;
+    private ArrayList<Habitacion> habitaciones;
+    private int numeroPisos=6;
+    private int numeroHabitaciones=10;
+    private double precioHabitacion=500;
+    
+    public AdminHotel(){
+        clientes= new ArrayList<Cliente>();
+        habitaciones= new ArrayList<Habitacion>();
+        CrearHabitaciones();
+    }
+
+    //GETTERS Y SETTERS
+    public int getNumeroHabitaciones() {
+        return numeroHabitaciones;
+    }
+
+    public void setNumeroHabitaciones(int numeroHabitaciones) {
+        this.numeroHabitaciones = numeroHabitaciones;
+    }
+
+    public double getPrecioHabitacion() {
+        return precioHabitacion;
+    }
+
+    public void setPrecioHabitacion(double precioHabitacion) {
+        this.precioHabitacion = precioHabitacion;
+    }
+    
+    public ArrayList<Cliente> getClientes() {
+        return clientes;
+    }
+
+    public void setClientes(ArrayList<Cliente> clientes) {
+        this.clientes = clientes;
+    }
+
+    public ArrayList<Habitacion> getHabitaciones() {
+        return habitaciones;
+    }
+
+    public void setHabitaciones(ArrayList<Habitacion> habitaciones) {
+        this.habitaciones = habitaciones;
+    }
+
+    public int getNumeroPisos() {
+        return numeroPisos;
+    }
+
+    public void setNumeroPisos(int numeroPisos) {
+        this.numeroPisos = numeroPisos;
+    }
+    
+    //FUNCIONES
+    
+    /**
+     * Crea todas las habitaciones del hotel y la guarda en el ArrayList habitaciones
+     */
+    public void CrearHabitaciones(){
+        for(int i=1;i<=numeroPisos;i+=1){
+            for(int j=1;i<=numeroHabitaciones;j+=1){
+                Habitacion nuevaHabitacion= new Habitacion();
+                nuevaHabitacion.setNumHab(j);
+                nuevaHabitacion.setPrecioHab(500);
+                switch(i){
+                    case 1:
+                        nuevaHabitacion.setPiso("A");
+                        break;
+                    case 2:
+                        nuevaHabitacion.setPiso("B");
+                        break;
+                    case 3:
+                        nuevaHabitacion.setPiso("C");
+                        break;
+                    case 4:
+                        nuevaHabitacion.setPiso("D");
+                        break;
+                    case 5:
+                        nuevaHabitacion.setPiso("E");
+                        break;
+                    case 6:
+                        nuevaHabitacion.setPiso("F");
+                        break;
+                    default:
+                        System.out.println("Ha habido un error creando las habitaciones");
+                }
+                habitaciones.add(nuevaHabitacion);
+            }
+        }
+        System.out.println("Se han creado todas las habitaciones con exito!");
+    }
+    
+    
+}
