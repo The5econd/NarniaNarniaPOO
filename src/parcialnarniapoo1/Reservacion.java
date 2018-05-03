5646182e2c7d68a06b6abd0bb4ae711508e53bd9
NeoCat98 <00076017@uca.edu.sv>
2018-05-02 21:52:17 -0600

    Se agrego la clase reservacion con sus atributos, su respectivo setters, getters y constructores

---
+++ src/parcialnarniapoo1/Reservacion.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parcialnarniapoo1;
+
+/**
+ *
+ * @author javie
+ */
+public class Reservacion {
+    public String infoHuesped;
+    public int numeroHabitacion;
+    public int diasReservacion;
+    public double costoNoche;
+    public double costoTotal;
+    public int paquete;
+
+    public String getInfoHuesped() {
+        return infoHuesped;
+    }
+
+    public void setInfoHuesped(String infoHuesped) {
+        this.infoHuesped = infoHuesped;
+    }
+
+    public int getNumeroHabitacion() {
+        return numeroHabitacion;
+    }
+
+    public void setNumeroHabitacion(int numeroHabitacion) {
+        this.numeroHabitacion = numeroHabitacion;
+    }
+
+    public int getDiasReservacion() {
+        return diasReservacion;
+    }
+
+    public void setDiasReservacion(int diasReservacion) {
+        this.diasReservacion = diasReservacion;
+    }
+
+    public double getCostoNoche() {
+        return costoNoche;
+    }
+
+    public void setCostoNoche(double costoNoche) {
+        this.costoNoche = costoNoche;
+    }
+
+    public double getCostoTotal() {
+        return costoTotal;
+    }
+
+    public void setCostoTotal(double costoTotal) {
+        this.costoTotal = costoTotal;
+    }
+
+    public int getPaquete() {
+        return paquete;
+    }
+
+    public void setPaquete(int paquete) {
+        this.paquete = paquete;
+    }
+    
+    public Reservacion() {
+    }
+
+    public Reservacion(String infoHuesped, int numeroHabitacion, int diasReservacion, double costoNoche, double costoTotal, int paquete) {
+        this.infoHuesped = infoHuesped;
+        this.numeroHabitacion = numeroHabitacion;
+        this.diasReservacion = diasReservacion;
+        this.costoNoche = costoNoche;
+        this.costoTotal = costoTotal;
+        this.paquete = paquete;
+    }
+
+}
